d78ce471083e45547302019a9923bbe157f07c2e
Daniel Miranda <daniel_miranda97@hotmail.com>
2022-06-26 19:47:35 -0300

    Ajustes de resize realizados

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,5 @@
+import debounce from './debounce.js';
+
 export class Slide{
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
@@ -8,6 +10,8 @@ export class Slide{
       startX: 0,
       movement: 0,
     }
+
+    this.activeClass = 'active';
   }
   
   transition(active) {
@@ -72,12 +76,6 @@ export class Slide{
     this.wrapper.addEventListener('mouseup', this.onEnd);
     this.wrapper.addEventListener('touchend', this.onEnd);
   }
-
-  bindEvents(){
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-  }
   
   //Slides config
 
@@ -114,6 +112,12 @@ export class Slide{
     this.moveSlide(activeSlide.position);
     this.slidesIndexNav(index);
     this.dist.finalPosition = activeSlide;
+    this.changeActiveClass();
+  }
+
+  changeActiveClass() {
+    this.slideArray.forEach(item => item.element.classList.remove(this.activeClass));
+    this.slideArray[this.index.active].element.classList.add(this.activeClass);
   }
 
   activePrevSlide() {
@@ -128,11 +132,30 @@ export class Slide{
     }
   }
   
+  onResize() {
+    setTimeout(() =>{
+      this.slidesConfig();
+      this.changeSlide(this.index.active);
+    },1000)
+  }
+
+  addResizeEvent() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  bindEvents(){
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    this.onResize = debounce(this.onResize.bind(this), 200)
+  }
+
   init() {
     this.bindEvents();
     this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
+    this.addResizeEvent();
 
     return this;
   }
